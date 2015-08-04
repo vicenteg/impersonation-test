75f1998f80b03d84f74e4aa95362ed0e10b49522
Vince Gonzalez <vince.gonzalez@gmail.com>
2015-08-04 14:29:07 +0000

    added sasl specific script and queries

--- create_query.sql
+++ create_query.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS pokes (foo INT, bar STRING);
+DROP TABLE IF EXISTS pokes;
+CREATE TABLE pokes (foo INT, bar STRING);
 LOAD DATA LOCAL INPATH '/opt/mapr/hive/hive-1.0/examples/files/kv1.txt'
 	OVERWRITE INTO TABLE pokes;
 SELECT * FROM pokes;
--- create_query2.sql
+++ create_query2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS pokes2 (foo INT, bar STRING);
+DROP TABLE IF EXISTS pokes2;
+CREATE TABLE pokes2 (foo INT, bar STRING);
 LOAD DATA LOCAL INPATH '/opt/mapr/hive/hive-1.0/examples/files/kv1.txt'
 	OVERWRITE INTO TABLE pokes2;
-SELECT * FROM pokes2;

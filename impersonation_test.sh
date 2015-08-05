5536cf280d5349c4e313a976103064ed39b8b605
Vince Gonzalez <vince.gonzalez@gmail.com>
2015-08-05 06:39:43 -0400

    remove trailing whitespace, fix creds for drop

--- impersonation_test.sh
+++ impersonation_test.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 ##
 # Create a table as the super user, then see that a non super user cannot
 # drop it.
-# 
+#
 # Create a table as a regular user, see that the regular user can create and
 # drop tables.
 ##
@@ -33,51 +33,51 @@ if [ $1 == "sasl" ]; then
 	JDBC_URL=$SASL_JDBC_URL
 	SUPERUSER_CREDS=""
 	USER_CREDS=""
-fi
 
-echo -n "test that $USER has a current ticket: "
-output=$(maprlogin print)
-if assert "$? -eq 0"; then
-	echo ok 
-else 
-	echo "$output"
-	exit 1 
-fi
+	echo -n "test that $USER has a current ticket: "
+	output=$(maprlogin print)
+	if assert "$? -eq 0"; then
+		echo ok
+	else
+		echo "$output"
+		exit 1
+	fi
 
-echo -n "test that $SUPERUSER has a current ticket: "
-output=$(maprlogin print)
-if assert "$? -eq 0"; then
-	echo ok 
-else 
-	echo "$output"
-	exit 1 
+	echo -n "test that $SUPERUSER has a current ticket: "
+	output=$(maprlogin print)
+	if assert "$? -eq 0"; then
+		echo ok
+	else
+		echo "$output"
+		exit 1
+	fi
 fi
 
 echo -n "create table as superuser: should exit 0 (success): "
 output=$(sudo -u $SUPERUSER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $SUPERUSER_CREDS -f $CREATE_QUERYFILE 2>&1 )
 if assert "$? -eq 0"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 echo -n "check that $SUPERUSER owns the table file: "
 owner=$(stat -c "%U" $WAREHOUSE_PATH/pokes);
 if  assert "$owner == $SUPERUSER"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 echo -n "create table as regular user: should exit 0 (success): "
 output=$(sudo -u $USER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $USER_CREDS -f $CREATE_QUERYFILE2 2>&1 )
 if assert "$? -eq 0"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 echo -n "check that $USER owns the table file: "
@@ -92,46 +92,46 @@ fi
 echo -n "submit query as $USER that triggers mapreduce job: "
 output=$(sudo -u $USER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $USER_CREDS -f $MAPREDUCE_QUERY2 2>&1 )
 if assert "$? -eq 0"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 echo -n "submit query as $SUPERUSER that triggers mapreduce job: "
 output=$(sudo -u $SUPERUSER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $USER_CREDS  -f $MAPREDUCE_QUERY 2>&1 )
 if assert "$? -eq 0"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 
 echo -n "drop table created by regular user as regular user. Should exit 0 (success): "
 output=$(sudo -u $USER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $USER_CREDS -f $DROP_QUERYFILE2 2>&1 )
 if assert "$? -eq 0"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 echo -n "drop table created by superuser as regular user. Should exit 2 (fail): "
-output=$(sudo -u $USER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $SUPERUSER_CREDS -f $DROP_QUERYFILE 2>&1 )
+output=$(sudo -u $USER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $USER_CREDS -f $DROP_QUERYFILE 2>&1 )
 if assert "$? -eq 2"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
 echo -n "drop table create by superuser as superuser. Should exit 0 (success): "
 output=$(sudo -u $SUPERUSER /opt/mapr/hive/hive-1.0/bin/beeline -u "$JDBC_URL" $SUPERUSER_CREDS -f $DROP_QUERYFILE 2>&1 )
 if assert "$? -eq 0"; then
-	echo ok 
-else 
+	echo ok
+else
 	echo "$output"
-	exit 1 
+	exit 1
 fi
 
